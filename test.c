de0ca0c16d8d35e6dd160d62772be8a5e7e151d5
manticorexl <zip3366@naver.com>
2025-12-29 16:38:07 +0900

    Third day commit 3

--- test.c
+++ test.c
@@ -1,49 +1,23 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void) {
 
-    double i_load, vel, R;
-    double V, C, eta;
+    double x, y;
 
-    // Get environment information
-    printf("Average current load (A): ");
-    scanf("%lf", &i_load);
-    printf("Average velocity (km/h): ");
-    scanf("%lf", &vel);
-    printf("Remaining percent (0-1)): ");
-    scanf("%lf", &R);
+    // Get x and y
+    printf("Input x: ");
+    scanf("%lf", &x);
+    printf("Input y: ");
+    scanf("%lf", &y);
 
-    // exception catch for R
-    if((i_load < 0) || (vel < 0) || (R > 1) || (R < 0)) {
-        printf("The range of R must be in [0, 1].\n");
-        return -1;
-    }
-
-    // Get battery information
-    printf("Factory voltage(V): ");
-    scanf("%lf", &V);
-    printf("Total capacity (Ah): ");
-    scanf("%lf", &C);
-    printf("System efficiency = eta (0-1): ");
-    scanf("%lf", &eta);
-
-    // exception catch for eta
-    if((V < 0) || (C < 0) || (eta > 1) || (eta < 0)) {
-        printf("The range of eta must be in [0, 1].\n");
-        return -1;
-    }
-    
-    // Calculate usable energy, power consumption, runtime, distance
-    double e_u = V * C * eta * (1 - R);
-    double p = (V * i_load) / eta;
-    double t = e_u / p;
-    double d = vel * t;
-
-    // Print the calculated results
-    printf("Total usable energy (Wh): %.2lf\n", e_u);
-    printf("Power consumption (W): %.2lf\n", p);
-    printf("Total runtime (h): %.2lf\n", t);
-    printf("Total range (km): %.2lf\n", d);
+    // Print outputs from math functions
+    printf("sqrt(x): %lf\n", sqrt(x));
+    printf("pow(x, y): %lf\n", pow(x, y));
+    printf("sin(x): %lf\n", sin(x));
+    printf("cos(x): %lf\n", cos(x));
+    printf("tan(x): %lf\n", tan(x));
+    printf("fabs(x): %lf\n", fabs(x));
 
     return 0;
 }
