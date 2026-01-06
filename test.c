5089bab00fd44396701277756fc0a7f68d7cde67
manticorexl <zip3366@naver.com>
2026-01-06 16:49:07 +0900

    26/01/26 commit

--- test.c
+++ test.c
@@ -12,12 +12,24 @@ This program
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <time.h>
-
 
 int main(void) {
 
-    
-    
+    char animal[20];
+    char reason[32];
+
+    printf("What's your favorite animal?: ");
+    scanf("%s", animal);
+
+    getchar();
+    printf("Why do you like that animal?: ");
+    fgets(reason, sizeof(reason), stdin);
+
+    reason[strlen(reason) - 1] = '\0';
+
+    printf("You like %s because it's %s.\n", animal, reason);
+
     return 0;
 }
