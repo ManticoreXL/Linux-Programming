f6299d648ff1b0ca90bb121eba1721dea83fdea5
manticorexl <zip3366@naver.com>
2026-01-09 16:39:48 +0900

    26/01/09 commit 3

--- test.c
+++ test.c
@@ -18,9 +18,30 @@ This program
 #include <float.h>
 
 
+typedef struct Position {
+    int x;
+    int y;
+} Position;
+
+void move(Position *p, int dx, int dy) {
+    p->x += dx;
+    p->y += dy;
+
+    return;
+}
+
+
 int main(void) {
 
-    
-   
+    Position robot = {0, 0};
+
+    printf("Start position: x=%d, y=%d\n", robot.x, robot.y);
+
+    move(&robot, 5, 3);
+    printf("After first move: x=%d, y=%d\n", robot.x, robot.y);
+
+    move(&robot, -2, 4);
+    printf("After second move: x=%d, y=%d\n", robot.x, robot.y);
+
     return 0;
 }
